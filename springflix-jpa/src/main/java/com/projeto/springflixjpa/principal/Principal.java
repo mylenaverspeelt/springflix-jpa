10d78f81a9673a8f895aa70feaece16a5da9c8d7
mylenaverspeelt <mylena49@hotmail.com>
2023-12-01 10:03:12 -0300

    find serie by actors names

--- springflix-jpa/src/main/java/com/projeto/springflixjpa/principal/Principal.java
+++ springflix-jpa/src/main/java/com/projeto/springflixjpa/principal/Principal.java
@@ -12,119 +12,131 @@ import com.projeto.springflixjpa.service.ConsumoApi;
 import com.projeto.springflixjpa.service.ConverteDados;
 
 public class Principal {
-	private Scanner leitura = new Scanner(System.in);
-	private ConsumoApi consumo = new ConsumoApi();
-	private ConverteDados conversor = new ConverteDados();
-	private static final String ENDERECO = "https://www.omdbapi.com/?t=";
-	private static final String API_KEY = "&apikey=6585022c";
-	private List<SeriePersonalizada> listaSeriesBanco = new ArrayList<>();
-	private SerieRepository repositorio;
+    private Scanner leitura = new Scanner(System.in);
+    private ConsumoApi consumo = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();
+    private static final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private static final String API_KEY = "&apikey=6585022c";
+    private List<SeriePersonalizada> listaSeriesBanco = new ArrayList<>();
+    private SerieRepository repositorio;
 
-	public Principal(SerieRepository repositorio) {
-		this.repositorio = repositorio;
-	}
+    public Principal(SerieRepository repositorio) {
+        this.repositorio = repositorio;
+    }
 
-	public void exibeMenu() {
+    public void exibeMenu() {
 
-		var opcaoEscolhida = -1;
+        var opcaoEscolhida = -1;
 
-		while (opcaoEscolhida != 0) {
-			var menu = """
+        while (opcaoEscolhida != 0) {
+            var menu = """
                     1 - Cadastrar nova série no banco
                     2 - Listar séries salvas
                     3 - Buscar temporadas 
                     4 - Procurar série por trecho do nome
+                    5 - Buscar série pelo nome da atriz/ator
                     0 - Sair
                     """;
 
-			System.out.println(menu);
-			var opcao = leitura.nextInt();
-			leitura.nextLine();
+            System.out.println(menu);
+            var opcao = leitura.nextInt();
+            leitura.nextLine();
 
-			switch (opcao) {
-				case 1:
-					salvarSerieBanco();
-					break;
-				case 2:
-					listarSeriesBuscadas();
-					break;
-				case 3:
-					buscarTemporadasDaSerie();
-					break;
-				case 4:
-					buscarSeriePorParteDoTitulo();
-					break;
-				case 0:
-					System.out.println("Programa finalizado com sucesso!");
-					System.exit(0);
-					break;
-				default:
-					System.out.println("Opção inválida");
-			}
-		}
-	}
+            switch (opcao) {
+                case 1:
+                    salvarSerieBanco();
+                    break;
+                case 2:
+                    listarSeriesBuscadas();
+                    break;
+                case 3:
+                    buscarTemporadasDaSerie();
+                    break;
+                case 4:
+                    buscarSeriePorParteDoTitulo();
+                    break;
+                case 5:
+                    buscarSeriesPorAtor();
+                    break;
+                case 0:
+                    System.out.println("Programa finalizado com sucesso!");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+            }
+        }
+    }
 
-	private void buscarSeriePorParteDoTitulo() {
-		System.out.println("Digite o nome da série para busca:");
-		var nomeSerie = leitura.nextLine();
-		Optional<SeriePersonalizada> serieBuscada = repositorio.findByTituloContainingIgnoreCase(nomeSerie);
+    private void buscarSeriesPorAtor() {
+        System.out.println("Digite um nome de atriz/ator para busca: ");
+        var nomeAtor = leitura.nextLine();
+        List<SeriePersonalizada> seriesEncontradas = repositorio.findByAtoresContainingIgnoreCase(nomeAtor);
+        System.out.println("Séries realizadas: ");
+        seriesEncontradas.forEach(s -> System.out.println(s.getTitulo()));
 
-		if(serieBuscada.isPresent()){
-			System.out.println("Série encontrada: " + serieBuscada.get().getTitulo());
-		}else{
-			System.out.println("Série não encontrada. Adicione uma nova série (opção 1)!");
-		}
-	}
+    }
 
-	private void listarSeriesBuscadas() {
-		listaSeriesBanco = repositorio.findAll();
+    private void buscarSeriePorParteDoTitulo() {
+        System.out.println("Digite o nome da série para busca:");
+        var nomeSerie = leitura.nextLine();
+        Optional<SeriePersonalizada> serieBuscada = repositorio.findByTituloContainingIgnoreCase(nomeSerie);
 
-		listaSeriesBanco.stream().sorted(Comparator.comparing(SeriePersonalizada::getGenero))
-				.forEach(item -> System.out.println(item.getTitulo()));
-	}
+        if (serieBuscada.isPresent()) {
+            System.out.println("Série encontrada: " + serieBuscada.get().getTitulo());
+        } else {
+            System.out.println("Série não encontrada. Adicione uma nova série (opção 1)!");
+        }
+    }
 
-	private void salvarSerieBanco() {
-		SerieBase serieBuscadaAPI = buscarSerieWeb();
-		SeriePersonalizada novaSerie = new SeriePersonalizada(serieBuscadaAPI);
-		repositorio.save(novaSerie);
-		System.out.println("Série salva com sucesso: " + serieBuscadaAPI.titulo());
-	}
+    private void listarSeriesBuscadas() {
+        listaSeriesBanco = repositorio.findAll();
 
-	private SerieBase buscarSerieWeb() {
-		System.out.println("Digite o nome da série para busca:");
-		var nomeSerie = leitura.nextLine();
-		var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
-		SerieBase dadosSerie = conversor.obterDados(json, SerieBase.class);
-		return dadosSerie;
-	}
+        listaSeriesBanco.stream().sorted(Comparator.comparing(SeriePersonalizada::getGenero)).forEach(item -> System.out.println(item.getTitulo()));
+    }
 
-	private void buscarTemporadasDaSerie() {
+    private void salvarSerieBanco() {
+        SerieBase serieBuscadaAPI = buscarSerieWeb();
+        SeriePersonalizada novaSerie = new SeriePersonalizada(serieBuscadaAPI);
+        repositorio.save(novaSerie);
+        System.out.println("Série salva com sucesso: " + serieBuscadaAPI.titulo());
+    }
 
-		listarSeriesBuscadas();
-		System.out.println("Dentre as séries mostradas acima, digite qual delas você deseja ver as temporadas:");
-		String nomeSerie = leitura.nextLine();
+    private SerieBase buscarSerieWeb() {
+        System.out.println("Digite o nome da série para busca:");
+        var nomeSerie = leitura.nextLine();
+        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        SerieBase dadosSerie = conversor.obterDados(json, SerieBase.class);
+        return dadosSerie;
+    }
 
-		Optional<SeriePersonalizada> serieEncontradaBanco = repositorio.findByTituloContainingIgnoreCase(nomeSerie);
+    private void buscarTemporadasDaSerie() {
 
-		if (serieEncontradaBanco.isPresent()) {
+        listarSeriesBuscadas();
+        System.out.println("Dentre as séries mostradas acima, digite qual delas você deseja ver as temporadas:");
+        String nomeSerie = leitura.nextLine();
 
-			var serieEncontrada = serieEncontradaBanco.get();
+        Optional<SeriePersonalizada> serieEncontradaBanco = repositorio.findByTituloContainingIgnoreCase(nomeSerie);
 
-			List<Temporada> temporadas = new ArrayList<>();
+        if (serieEncontradaBanco.isPresent()) {
 
-			for (int i = 1; i <= serieEncontrada.getTotalTemporadas(); i++) {
-				var json = consumo.obterDados(ENDERECO + serieEncontrada.getTitulo().replace(" ", "+") + "&season=" + i + API_KEY);
-				Temporada temporada = conversor.obterDados(json, Temporada.class);
-				temporadas.add(temporada);
-			}
-			temporadas.forEach(System.out::println);
+            var serieEncontrada = serieEncontradaBanco.get();
 
-			List<EpisodioPersonalizado> episodiosPorTemporada = temporadas.stream().flatMap(d -> d.episodios().stream().map(e -> new EpisodioPersonalizado(d.numero(), e))).collect(Collectors.toList());
+            List<Temporada> temporadas = new ArrayList<>();
 
-			serieEncontrada.setListaEpisodios(episodiosPorTemporada);
-			repositorio.save(serieEncontrada);
-		} else {
-			System.out.println("Série não encontrada!");
-		}
-	}
+            for (int i = 1; i <= serieEncontrada.getTotalTemporadas(); i++) {
+                var json = consumo.obterDados(ENDERECO + serieEncontrada.getTitulo().replace(" ", "+") + "&season=" + i + API_KEY);
+                Temporada temporada = conversor.obterDados(json, Temporada.class);
+                temporadas.add(temporada);
+            }
+            temporadas.forEach(System.out::println);
+
+            List<EpisodioPersonalizado> episodiosPorTemporada = temporadas.stream().flatMap(d -> d.episodios().stream().map(e -> new EpisodioPersonalizado(d.numero(), e))).collect(Collectors.toList());
+
+            serieEncontrada.setListaEpisodios(episodiosPorTemporada);
+            repositorio.save(serieEncontrada);
+        } else {
+            System.out.println("Série não encontrada!");
+        }
+    }
 }
--- springflix-jpa/src/main/java/com/projeto/springflixjpa/repository/SerieRepository.java
+++ springflix-jpa/src/main/java/com/projeto/springflixjpa/repository/SerieRepository.java
@@ -5,10 +5,12 @@ import org.springframework.stereotype.Repository;
 
 import com.projeto.springflixjpa.model.SeriePersonalizada;
 
+import java.util.List;
 import java.util.Optional;
 
-
+//usar o nome do atributo quando for fazer uma derived querie, não o nome da coluna da tabela
 @Repository
-public interface SerieRepository extends JpaRepository<SeriePersonalizada, Long>{
-Optional<SeriePersonalizada>  findByTituloContainingIgnoreCase(String titulo);
+public interface SerieRepository extends JpaRepository<SeriePersonalizada, Long> {
+    Optional<SeriePersonalizada> findByTituloContainingIgnoreCase(String titulo);
+    List<SeriePersonalizada> findByAtoresContainingIgnoreCase(String nomeAtor);
 }
